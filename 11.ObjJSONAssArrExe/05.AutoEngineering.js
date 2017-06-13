6dfe68a7a5a6fec48884c4d23856c7be8fb5fe98
ivobar <babaginka@students.softuni.bg>
2017-06-13 23:13:56 +0300

    Solved 06.AutoEngineering

--- 11.ObjJSONAssArrExe/05.AutoEngineering.js
+++ 11.ObjJSONAssArrExe/05.AutoEngineering.js
@@ -1,3 +1,42 @@
-/**
- * Created by ivoba on 13.6.2017 г..
- */
+function solve(strArr) {
+    let brand = '';
+    let model = '';
+    let prodNum = 0;
+    let production = new Map();
+
+    for (let row of strArr) {
+        let data = row.split(' | ');
+        brand = data[0];
+        model = data[1];
+        prodNum = Number(data[2]);
+
+        if (production.has(brand)) {
+            if (production.get(brand).has(model)) {
+                production.get(brand).set(model, production.get(brand).get(model) + prodNum);
+            } else {
+                production.get(brand).set(model, prodNum);
+            }
+        } else {
+            production.set(brand,new Map());
+            production.get(brand).set(model,prodNum);
+        }
+    }
+
+    for (let [brand, modelData] of production) {
+        console.log(brand);
+        for (let [model, num] of modelData) {
+            console.log(`###${model} -> ${num}`);
+        }
+    }
+}
+
+solve(['Audi | Q7 | 1000',
+    'Audi | Q6 | 100',
+    'BMW | X5 | 1000',
+    'BMW | X6 | 100',
+    'Citroen | C4 | 123',
+    'Volga | GAZ-24 | 1000000',
+    'Lada | Niva | 1000000',
+    'Lada | Jigula | 1000000',
+    'Citroen | C4 | 22',
+    'Citroen | C5 | 10']);
